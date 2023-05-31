878ccb045d4604b40284fa7b5933065623f0c1f9
samigozz <samuel.mirandag2000@gmail.com>
2023-05-31 20:52:13 +0200

    Cinemachine and Paralax sprites

--- Assets/_Data/Scripts/Parallax.cs
+++ Assets/_Data/Scripts/Parallax.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    
+    [SerializeField] Camera cam;
+    [SerializeField] Transform subject;
+
+    Vector2 startPosition;
+    Vector2 travel => (Vector2)cam.transform.position - startPosition;
+    Vector2 paralaxFactor;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+    private void Update()
+    {
+        transform.position = startPosition + travel;
+    }
 }
