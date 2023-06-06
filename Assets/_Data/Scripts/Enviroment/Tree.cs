4e00439bd6df946b13c46e67db3fcf8c8d3e3221
samigozz <samuel.mirandag2000@gmail.com>
2023-06-06 21:01:15 +0200

    DOTween - y tala de arboles

--- Assets/_Data/Scripts/Enviroment/Tree.cs
+++ Assets/_Data/Scripts/Enviroment/Tree.cs
@@ -1,28 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class Tree : MonoBehaviour, IInteractable
 {
-    [SerializeField] Sprite grownsprite, cutSprite;
+    [SerializeField] SpriteRenderer treeSR, stumpSR;
     private bool isGrown =true;
+
+    private int health = 3;
+
+    [Header("Animation")]
+
+    [SerializeField] Vector3 shakeStrength;
 
     SpriteRenderer sr;
 
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
-        sr.sprite = grownsprite;
+        stumpSR.gameObject.SetActive(false);
     }
 
     public void Interact()
     {
-        Debug.Log("Cut.");
-        sr.sprite = cutSprite;
-        isGrown = false;
+        Shake();
+        health--;
+        Debug.Log("Hit");
+        if (health <= 0)
+        {
+            stumpSR.gameObject.SetActive(true);
+            treeSR.gameObject.SetActive(false);
+            isGrown = false;
+        }
+
     }
     public bool CanInteract()
     {
         return isGrown;
+    }
+
+    private void Shake()
+    {
+        transform.DOShakeRotation(1f, shakeStrength);
+    }
+
+    private void Fall()
+    {
+       
     }
 }
