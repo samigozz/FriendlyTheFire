78bf172cce7ace9d53c53db55e0e92c1d04948db
samigozz <samuel.mirandag2000@gmail.com>
2023-06-28 19:41:51 +0200

    Fuego con particulas y fixes finales

--- Assets/_Data/Scripts/Friendly/FriendlyHeal.cs
+++ Assets/_Data/Scripts/Friendly/FriendlyHeal.cs
@@ -9,23 +9,28 @@ public class FriendlyHeal : MonoBehaviour
     [SerializeField]
     private IntValue playerLives;
 
+    [SerializeField] 
+    private GameObject KeyPrompt;
+
     private bool inRange;
 
     private void Start()
     {
         heal = 1;
         inRange = false;
+        KeyPrompt.SetActive(false);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         inRange = true;
-        
+        KeyPrompt.SetActive(true);
         StartCoroutine(FriendlyHealPlayer(collision));
     }
     
     private void OnTriggerExit2D(Collider2D collision)
     {
         inRange = false; 
+        KeyPrompt.SetActive(false);
     }
 
     private IEnumerator FriendlyHealPlayer(Collider2D collision)
