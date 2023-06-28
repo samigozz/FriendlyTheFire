47377aa9dcf991c5041ceb9b510051a75180fbad
Manuel <manuelgalvez192@gmail.com>
2023-06-28 02:47:26 +0200

    se perdio el interactable del fuego, se lo pongo de nuevo

--- Assets/_Data/Scripts/Player/WoodToFriendly.cs
+++ Assets/_Data/Scripts/Player/WoodToFriendly.cs
@@ -26,7 +26,7 @@ public class WoodToFriendly : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var interactable = collision.GetComponent<IInteractable>();
-        
+
         if (interactable != null && interactable.CanInteract())
         {
             interactablesInRange.Add(interactable);
